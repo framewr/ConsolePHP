c514f31431b5d7b1e302c4afbbf1f9b5788a7d51
Yaroslav <bars909@mail.ru>
2014-11-24 21:47:05 +0200

    Create singleton.php

---
+++ singleton.php
@@ -0,0 +1,24 @@
+class Singleton
+{
+    private static $instance = null;
+    private $db;
+    
+    private function __clone() {}
+    private function __construct($db_name = 'comments', $host = 'localhost', $user = 'root', $password= '') {
+      $this->db = new mysqli($host, $user, $password, $db_name);
+    }
+   
+    public static function getInstance()
+    {
+        if (null === self::$instance)
+        {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+    
+    public static function close_connection()
+    {
+        self::$mysqli->close();
+    }
+}
