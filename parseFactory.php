b4bab2d1cdd90201ce897cc3c827551a66ed0ffd
Yaroslav <bars909@mail.ru>
2014-11-25 12:15:20 +0200

    Create parseFactory.php

---
+++ parseFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+class parseFactory
+{
+  public static function Create()
+  {
+     return new Article( $id );
+  }
+}
+$ao = ArticleFactory::Create( 1 );
+echo( $ao->getTitle()."\n" );
+
+?>
