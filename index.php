f129e6b5e1c06ac974cbf93e5fced7d802b8ad53
Yaroslav <bars909@mail.ru>
2014-11-25 20:04:14 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 #!/usr/bin/php
 <?php
+//Подключаем Factory
+require_once('Factory.php');
 
+// Проверяем, правильно ли передано название файла
 if ($argc != 2 || in_array($argv[1], array('--help', '-help', '-h', '-?'))) {
-      
+      echo "The incorrect path to the file!";
 } else {
-    echo $argv[1];
+      $file = Factory::CreateFile($argv[1]);
 }
 
 
