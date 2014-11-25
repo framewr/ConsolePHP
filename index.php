f4da3f9449d2b275ea0475e4423770cdf04737be
Yaroslav <bars909@mail.ru>
2014-11-25 20:11:36 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ if ($argc != 2 || in_array($argv[1], array('--help', '-help', '-h', '-?'))) {
       echo "The incorrect path to the file!";
 } else {
       $file = Factory::CreateFile($argv[1]);
+      
 }
 
 
