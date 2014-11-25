f2f7c7fb5bba5e4ca57c6b9953e0c05ce0e6ad5c
Yaroslav <bars909@mail.ru>
2014-11-25 12:12:47 +0200

    Create parse.php

---
+++ parse.php
@@ -0,0 +1,24 @@
+<?php
+
+//Подключаем файл интерфейсов
+require_once('interface.php');
+
+class Parse implements IHandler
+{
+    public function __construct( $id ) { }
+  public function getTitle()
+  {
+    return "Blog Article";
+  }
+}
+class ArticleFactory
+{
+  public static function Create( $id )
+  {
+     return new Article( $id );
+  }
+}
+$ao = ArticleFactory::Create( 1 );
+echo( $ao->getTitle()."\n" );
+
+?>
