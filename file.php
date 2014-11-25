39d05f68b3b21e12014b560df9f1f612d94127dc
Yaroslav <bars909@mail.ru>
2014-11-25 20:22:20 +0200

    Update file.php

--- file.php
+++ file.php
@@ -1,23 +1,31 @@
 <?php
+
+// Подключаем Factory для работы с Parse
+require_once('Factory.php');
+
 // Создаем клас для получения содержимого файлов текстур
 class File
 {
-      public $fileIni;
-      public $fileJson;
+      public $file_type;
+      public $file_parse;
       
       public function __construct($file_name)
       {
             $file_type_array = explode(".", $file_name);
             
             if($file_type_array[1] == 'ini') {
-                  $this->fileIni = file_get_contents($file_name);
-                  return $this->fileIni;                        
+                  $this->file_type = file_get_contents($file_name);
+                  return $this->file_type;                        
             } elseif ($file_type_array[1] == 'json') {
-                  $this->fileJson = file_get_contents($file_name);
-                  return $this->fileJson;
+                  $this->file_type = file_get_contents($file_name);
+                  return $this->file_type;
             } else {
                   throw new Exception('Передан файл неверного формата! Пожалуста повторите попытку снова.');
-            }                        
+            }
+            
+            // Парсим содержимое файла
+            $obj = Factory::CreateParse();
+            $this->file_parse = $obj
       }
 }
 
