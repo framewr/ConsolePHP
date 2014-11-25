f4cb6008ea257c8baaf6720b60abddb59ec3b0fa
Yaroslav <bars909@mail.ru>
2014-11-25 22:51:08 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,10 +8,19 @@ require_once('activeRecord_Post.php');
 require_once('activeRecord_User.php');
 
 // Проверяем, правильно ли передано название файла
-if ($argc != 2 || in_array($argv[1], array('--help', '-help', '-h', '-?'))) {
+if ($argc != 2 || !is_array($argv[1])) {
       echo "The incorrect path to the file!";
 } else {
       $file_parse_array = Factory::CreateFile($argv[1]);
+      
+      //ПРоверяем существует ли запись в бд с id
+      if(isset(OrmActiveRecord_User::find($file_parse_array->id))) {
+            // Если существует - делаем update записи
+            if(OrmActiveRecord_User::save($file_parse_array)) {
+                  echo "Данные успешно обновлены";
+            }
+      }
+      
       
 }
 
