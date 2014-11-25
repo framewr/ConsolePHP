9a278e41ec900d6667d3357cc2efb05c5e17ac90
Yaroslav <bars909@mail.ru>
2014-11-25 20:49:53 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,11 +3,14 @@
 //Подключаем Factory
 require_once('Factory.php');
 
+//Подключаем ActiveRecord
+require_once('activeRecord.php');
+
 // Проверяем, правильно ли передано название файла
 if ($argc != 2 || in_array($argv[1], array('--help', '-help', '-h', '-?'))) {
       echo "The incorrect path to the file!";
 } else {
-      $file = Factory::CreateFile($argv[1]);
+      $file_parse_array = Factory::CreateFile($argv[1]);
       
 }
 
