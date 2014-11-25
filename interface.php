62fdb4f889517b5fd1d5daa59f447a30c49514e5
Yaroslav <bars909@mail.ru>
2014-11-25 12:11:46 +0200

    Update interface.php

--- interface.php
+++ interface.php
@@ -1,6 +1,7 @@
 <?php
+
 // Объявим интерфейс 'iHandler'
-interface iHandler
+interface IHandler
 {
     //Парсим файлы с расширением *.ini
     public function parseIni();
@@ -8,4 +9,5 @@ interface iHandler
     //Парсим файлы с расширением *.json
     public function parseJson();
 }
+
 ?>
