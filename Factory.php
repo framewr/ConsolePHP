a328b0160848cb74a5aaed03b4f7f44a84899da0
Yaroslav <bars909@mail.ru>
2014-11-25 19:16:17 +0200

    Update and rename parseFactory.php to Factory.php

---
+++ Factory.php
@@ -0,0 +1,21 @@
+<?php
+
+//Подключаем классы для factory
+require_once('parse.php');
+require_once('file.php');
+
+class Factory
+{
+    public static function CreateParse()
+    {
+        return new Parse();
+    }
+    
+    public static function CreateFile($file_name)
+    {
+        return new File($file_name);
+    }
+}
+
+
+?>
