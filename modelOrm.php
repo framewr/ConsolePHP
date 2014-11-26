4c672029928937ae80d0829c443102c6e4bf949f
Yaroslav <bars909@mail.ru>
2014-11-26 21:50:51 +0200

    Update modelOrm.php

--- modelOrm.php
+++ modelOrm.php
@@ -8,8 +8,12 @@ class modelOrm extends  OrmActiveRecord
     protected $table = 'news';
     protected static $tableName = 'news';
     public $fieldsList = array('id', 'title', ‘date’, ‘content’);
-
-
+    
+    public function __construct($table, $array)
+    {
+        self::table = '$table';
+        $this->fieldsList = $array;
+    }
 }
 
 ?>
