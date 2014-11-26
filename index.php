babee954f416925fa6fe210232c2cd13caa00e45
Yaroslav <bars909@mail.ru>
2014-11-26 20:42:55 +0200

    Update index.php

--- index.php
+++ index.php
@@ -11,18 +11,12 @@ if ($argc != 2 || !is_array($argv[1])) {
       echo "The incorrect path to the file!";
 } else {
       $file_parse_array = Factory::CreateFile($argv[1]);
-      
-      //ПРоверяем существует ли запись в бд с id
-      if(OrmActiveRecord_User::find($file_parse_array->id) !=== NULL) {
+      //ПРоверяем существует ли запись в бд с id и сохраняем данные в бд (insert или update)
+      if(OrmActiveRecord::find($file_parse_array->id) !=== NULL) {
             // Если существует - делаем update записи
-            if(OrmActiveRecord_User::save($file_parse_array) == true) {
-                  echo "Данные успешно обновлены";
+            if(OrmActiveRecord::save($file_parse_array) == true) {
+                  echo "Данные успешно сохранены";
             } 
-      } else {
-            // Иначе добавляем новую запись в таблицу бд
-            if(OrmActiveRecord_User::insert($file_parse_array) == true) {
-                  echo "Данные успешно добавлены в бд";      
-            }
       }
 }
 
