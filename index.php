1e2dc0194291e5b87498e4ab55f6fa573d0a3f97
Yaroslav <bars909@mail.ru>
2014-11-26 07:34:09 +0200

    Update index.php

--- index.php
+++ index.php
@@ -14,14 +14,17 @@ if ($argc != 2 || !is_array($argv[1])) {
       $file_parse_array = Factory::CreateFile($argv[1]);
       
       //ПРоверяем существует ли запись в бд с id
-      if(isset(OrmActiveRecord_User::find($file_parse_array->id))) {
+      if(OrmActiveRecord_User::find($file_parse_array->id) !=== NULL) {
             // Если существует - делаем update записи
-            if(OrmActiveRecord_User::save($file_parse_array)) {
+            if(OrmActiveRecord_User::save($file_parse_array) == true) {
                   echo "Данные успешно обновлены";
+            } 
+      } else {
+            // Иначе добавляем новую запись в таблицу бд
+            if(OrmActiveRecord_User::insert($file_parse_array) == true) {
+                  echo "Данные успешно добавлены в бд";      
             }
       }
-      
-      
 }
 
 
