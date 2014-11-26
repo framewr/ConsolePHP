87859093d6dfb1226166d4fbea96cbf76fbe8908
Yaroslav <bars909@mail.ru>
2014-11-26 21:01:28 +0200

    Create modelOrm.php

---
+++ modelOrm.php
@@ -0,0 +1,22 @@
+<?php
+
+//Подключаем ActiveRecord
+require_once('activeRecord.php');
+
+class modelOrm extends  OrmActiveRecord
+{
+    protected $table = 'news';
+
+ 
+
+         protected static $tableName = 'news';
+
+ 
+
+         public $fieldsList = array('id', 'title', ‘date’, ‘content’);
+
+ 
+
+}
+
+?>
