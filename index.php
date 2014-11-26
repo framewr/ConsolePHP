ae176fb1428dba415c289a9653fd57d0a6c84c78
Yaroslav <bars909@mail.ru>
2014-11-26 22:00:46 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,11 +10,22 @@ require_once('activeRecord.php');
 if ($argc != 2 || !is_array($argv[1])) {
       echo "The incorrect path to the file!";
 } else {
-      $file_parse_array = Factory::CreateFile($argv[1]);
+      //Разбиваем файл для получения его типа формата
+      $file_type_array = explode(".", $argv[1]);
+      $file_parse_array = Factory::CreateFile($argv[1], $file_type_array[1]);
+      
+      if($file_type_array[0] =='post') {
+            $save = Factory::CreateModelActiveRecord('post', $file_parse_array)
+      } elseif($file_type_array[0] =='user') {
+            $save = Factory::CreateModelActiveRecord('user', $file_parse_array)
+      } else {
+            throw new Exception('Передан файл должен иметь имя 'post' или 'user'!);
+      }
+      
       //ПРоверяем существует ли запись в бд с id и сохраняем данные в бд (insert или update)
-      if(OrmActiveRecord::find($file_parse_array->id) !=== NULL) {
+      if(OrmActiveRecord::find() !=== NULL) {
             // Если существует - делаем update записи
-            if(OrmActiveRecord::save($file_parse_array) == true) {
+            if($save->save() == true) {
                   echo "Данные успешно сохранены";
             } 
       }
